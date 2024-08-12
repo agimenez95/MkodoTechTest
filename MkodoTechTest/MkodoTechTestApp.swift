6eb5e0439e23721d57a86dd24bfa605830feba44
Adriano Gimenez <13178341+agimenez95@users.noreply.github.com>
2024-08-12 15:00:38 +0100

    Initial Commit

---
+++ MkodoTechTest/MkodoTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MkodoTechTestApp.swift
+//  MkodoTechTest
+//
+//  Created by Adriano Gimenez on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MkodoTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
